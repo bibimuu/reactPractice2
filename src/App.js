3a2798b1d3673e38498611dc30809f841cca5390
Daigoro <48868808+bibimuu@users.noreply.github.com>
2021-11-21 17:27:03 +0900

    useCallbackとuseMemo、useEffectの実験

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo, useEffect } from "react";
 import "./styles.css";
 
 import { ChildArea } from "./ChildArea";
@@ -11,12 +11,21 @@ export default function App() {
     return setText(event.target.value);
   };
   const openToggle = () => setOpen(!open);
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  console.log(0);
+  const temp = useMemo(() => console.log(1), [setOpen]);
+  console.log(2);
+  const a = useEffect(() => console.log(3), []);
+  console.log(4);
+  const b = useCallback(() => console.log(5), []);
+  console.log(6);
 
   return (
     <>
       <input value={text} onChange={changeText} />
       <button onClick={openToggle}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,13 +1,14 @@
 import { memo } from "react";
 
 export const ChildArea = memo((props) => {
-  const { open } = props;
+  const { open, onClickClose } = props;
   console.log(9);
   return (
     <>
       {open && (
         <div>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       )}
     </>
